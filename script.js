1bdbbc3dd3f361b9cfef896a5196b06956136279
Eduardo Campos <edumachadocampos@gmail.com>
2022-01-26 01:31:54 -0300

    Resposta 1 e 2 atualizadas

---
+++ script.js
@@ -0,0 +1,32 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+
+for (let count = 1; count <= 10; count++) {
+    console.log(count)
+}
+
+let count2 = 10
+while (count2 >= 1) {
+    console.log(count2)
+    count2 -= 1
+}
+
+let impares = 1
+if (impares % 2 == 1 | impares == 1) {
+    console.log(impares);
+    impares += 1;
+}
+
+let pares = 0
+if (pares % 2 == 0) {
+    console.log(pares);
+    pares += 1;
+}
